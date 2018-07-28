3d53372f4418696000c395248ff3d90c56abd9a9
admin <admin001@qq.com>
2018-07-28 10:25:14 +0800

    开发公司模块

--- src/main/java/com/xdf/controller/HtmlController.java
+++ src/main/java/com/xdf/controller/HtmlController.java
@@ -10,11 +10,39 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HtmlController {
     /**
-     **欢迎页面
+     **领导欢迎页面
      * @return
      */
-    @RequestMapping("welcome.do")
-    public String index(){
-        return "welcome";
+    @RequestMapping("ld/welcome.do")
+    public String welcome(){
+        return "administration/welcome";
     }
+
+    /**
+     **公司管理页面
+     * @return
+     */
+    @RequestMapping("ld/management.do")
+    public String management(){
+        return "administration/management";
+    }
+
+    /**
+     **t添加公司管理页面
+     * @return
+     */
+    @RequestMapping("ld/addmanagement.do")
+    public String addmanagement(){
+        return "administration/management-add";
+    }
+
+    /**
+     * 领导首页
+     * @return
+     */
+    @RequestMapping("AdminIndex.do")
+    public String AdminIndex(){
+        return "administration/index";
+    }
+
 }
